1a4defbc7a10cc10523535f3717614c7ae9a424c
Javi <javisr997@gmail.com>
2020-07-23 14:26:44 +0200

    Clases Fanny y Jordi

---
+++ Clases Fanny/Condicionales y Bucles/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // If - else if - else
+        //// Operadores lógicos y más condicionales
+
+        // While (int, boolean, String)
+
+        // For
+
+        // Estructuras
+        //// Cadenas
+        ////// Funciones (equals, length, contains, indexOf, ...)
+        //// Arrays
+        //// Listas
+    }
+}
