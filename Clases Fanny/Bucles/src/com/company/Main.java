98481198202efabf01ef1d4b3ddf5928bc5a3a37
Javi <javisr997@gmail.com>
2020-07-28 17:11:18 +0200

    Clases Jordi y Fanny

---
+++ Clases Fanny/Bucles/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // Convertir variables
+
+        // While (int, boolean, String)
+
+        // For
+
+        // Switch
+    }
+}
--- Clases Jordi/ClasesObjetos/src/com/company/Main.java
+++ Clases Jordi/ClasesObjetos/src/com/company/Main.java
@@ -2,7 +2,22 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        // Clases y objetos
+
+        //// Atributos
+
+
+        //// Constructores
+
+
+        //// Métodos
+
+
+        //// Propiedades
+
+
+        // (encapsulamiento, polimorfismo, herencia, abstracción)
     }
 }
